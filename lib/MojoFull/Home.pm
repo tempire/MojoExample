2012429c275f7aa2a439096b349ff9d57a4176d0
Glen Hinkle <glen@empireenterprises.com>
2012-02-07 12:12:55 -0800

    changed home page to README.md content from githubs

--- lib/MojoFull/Home.pm
+++ lib/MojoFull/Home.pm
@@ -2,15 +2,7 @@ package MojoFull::Home;
 use Mojo::Base 'Mojolicious::Controller';
 
 sub index {
-  my $self = shift;
-
-  $self->render(
-    'home/index',
-    blog   => $self->db->resultset('Blog')->latest,
-    photos => [
-      $self->db->resultset('Photoset')->first->photos->latest(2)
-    ]
-  );
+  shift->render('home/index');
 }
 
 1;
